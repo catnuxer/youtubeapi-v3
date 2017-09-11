cacb1e6d452ed8405680e65b34a53c6fe1dc4f8d
Chuli jimmi Manurung <jimmi.ext@gmail.com>
2017-09-12 04:34:00 +0700

    create response sample

---
+++ Response.js
@@ -0,0 +1,91 @@
+{
+  error: 0,
+  data: {
+    kind: "youtube#videoListResponse",
+    etag: ""sNu6csVZt536JdlmpOxN9WQSd8U/3JHGInO6DGWXjgaHYNN4zIafckQ"",
+    pageInfo: {
+    totalResults: 1,
+    resultsPerPage: 1
+    },
+    items: [
+      {
+        kind: "youtube#video",
+        etag: ""sNu6csVZt536JdlmpOxN9WQSd8U/KFV3MRJ0WYrGEZJhJxkDEk5FaGI"",
+        id: "uvLSj4gRq-Q",
+        snippet: {
+          publishedAt: "2014-08-12T13:21:15.000Z",
+          channelId: "UCVp3nfGRxmMadNDuVbJSk8A",
+          title: "Wiz Khalifa - So High ft. Ghost Loft [Official Audio]",
+          description: "#BlaccHollywood August 19th, preorder now http://smarturl.it/blacchollywood See Wiz in your city http://wizkhalifa.com/tour Follow Wiz: http://twitter.com/wizkhalifa http://instagram.com/mistercap http://facebook.com/wizkhalifa http://soundcloud.com/wizkhalifa http://wizkhalifa.com http://taylorgangent.com Wiz Khalifa -- So High ft. Ghost Loft [Official Audio]",
+          thumbnails: {
+            default: {
+              url: "https://i.ytimg.com/vi/uvLSj4gRq-Q/default.jpg",
+              width: 120,
+              height: 90
+            },
+            medium: {
+              url: "https://i.ytimg.com/vi/uvLSj4gRq-Q/mqdefault.jpg",
+              width: 320,
+              height: 180
+            },
+            high: {
+              url: "https://i.ytimg.com/vi/uvLSj4gRq-Q/hqdefault.jpg",
+              width: 480,
+              height: 360
+            },
+            standard: {
+              url: "https://i.ytimg.com/vi/uvLSj4gRq-Q/sddefault.jpg",
+              width: 640,
+              height: 480
+            },
+            maxres: {
+              url: "https://i.ytimg.com/vi/uvLSj4gRq-Q/maxresdefault.jpg",
+              width: 1280,
+              height: 720
+            }
+          },
+          channelTitle: "Wiz Khalifa",
+          tags: [
+            "Wiz Khalifa",
+            "Blacc Hollywood",
+            "Taylor Gang",
+            "We Dem Boyz",
+            "Rostrum Records",
+            "Atlantic Records",
+            "Black Hollywood",
+            "hip hop",
+            "wiz",
+            "khalifa",
+            "blacchollywood",
+            "Pittsburgh",
+            "Los Angeles",
+            "taylorgangent",
+            "so high",
+            "ghost loft"
+          ],
+          categoryId: "10",
+          liveBroadcastContent: "none",
+          localized: {
+            title: "Wiz Khalifa - So High ft. Ghost Loft [Official Audio]",
+            description: "#BlaccHollywood August 19th, preorder now http://smarturl.it/blacchollywood See Wiz in your city http://wizkhalifa.com/tour Follow Wiz: http://twitter.com/wizkhalifa http://instagram.com/mistercap http://facebook.com/wizkhalifa http://soundcloud.com/wizkhalifa http://wizkhalifa.com http://taylorgangent.com Wiz Khalifa -- So High ft. Ghost Loft [Official Audio]"
+          }
+        },
+        contentDetails: {
+          duration: "PT4M10S",
+          dimension: "2d",
+          definition: "hd",
+          caption: "false",
+          licensedContent: true,
+          projection: "rectangular"
+        },
+        statistics: {
+          viewCount: "58115747",
+          likeCount: "289080",
+          dislikeCount: "12513",
+          favoriteCount: "0",
+          commentCount: "9887"
+        }
+      }
+    ]
+  }
+}
