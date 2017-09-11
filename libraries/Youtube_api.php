8176aceb7835d64af5ce664f4fe1dc7f66ad5fbb
Chuli jimmi Manurung <jimmi.ext@gmail.com>
2017-09-12 04:49:22 +0700

    create youtube api libraries

---
+++ libraries/Youtube_api.php
@@ -0,0 +1,29 @@
+<?php
+/*
+* Write by catnuxer
+* Email jimmi.ext@gmail.com
+* Feel free to use :thumsup
+*/
+require_once "youtube/class.youtube.api.php";
+
+class Youtube_api
+{
+	protected $lib;
+
+	public function __construct()
+  {
+		$this->lib=new youtubeApiService();
+    $ci=&get_instance();
+		$this->lib->set_param(YOUR_YOUTUBE_API_KEY_V3); //Set your youtube api key v3 here.
+  }
+
+	public function get_videoId($data)
+	{
+		return $this->lib->search($data);
+	}
+
+	public function get_multiple_videoId($data)
+	{
+		return $this->lib->search_multiple($data);
+	}
+}
